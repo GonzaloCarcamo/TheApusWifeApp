caa5c137aea464d0d9d71a8969bc8cf5f3200983
GonzaloCarcamo <gon.cmo.dev@gmail.com>
2020-02-09 02:58:08 -0300

    FUNCTIONS OK. LACK STYLE

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+Baby.where(name: 'Kiran').first_or_create
+Baby.where(name: 'Abdul').first_or_create
+Baby.where(name: 'Raj').first_or_create
+Baby.where(name: 'Apu').first_or_create
+Baby.where(name: 'Brahma').first_or_create
+Baby.where(name: 'Nevdar').first_or_create
+Baby.where(name: 'Hari').first_or_create
+Baby.where(name: 'Ravindranat').first_or_create
